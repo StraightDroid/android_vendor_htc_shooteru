f9e793fcfb89ac8b5af71ac5383d7e631097706e
StraightDroid <straightdroid@gmail.com>
2015-11-17 21:55:42 +0100

    fix build with qcom opengl

--- qcom/shooteru/device-shooteru.mk
+++ qcom/shooteru/device-shooteru.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/qmon/shooteru
+LOCAL_PATH := vendor/qcom/shooteru
 # Qualcomm blob(s) necessary for shooteru hardware
 PRODUCT_COPY_FILES += \
 \
@@ -20,7 +20,7 @@ $(LOCAL_PATH)/adreno2xx/system/lib/libC2D2.so:system/lib/libC2D2.so \
 $(LOCAL_PATH)/adreno2xx/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
 $(LOCAL_PATH)/adreno2xx/system/lib/libgsl.so:system/lib/libgsl.so \
 $(LOCAL_PATH)/adreno2xx/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
-$(LOCAL_PATH)/adreno2xx/system/lib/libsc-a2xx.so:system/lib/ libsc-a2xx.so \
+$(LOCAL_PATH)/adreno2xx/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
 \
 $(LOCAL_PATH)/adreno2xx/system/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
 $(LOCAL_PATH)/adreno2xx/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
@@ -31,5 +31,5 @@ $(LOCAL_PATH)/adreno2xx/system/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/
 $(LOCAL_PATH)/adreno2xx/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
 \
 $(LOCAL_PATH)/adreno2xx/system/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
-$(LOCAL_PATH)/adreno2xx/system/etc/firmware/leia_pm4_470.fw:system/etc/firmware/ leia_pm4_470.fw
+$(LOCAL_PATH)/adreno2xx/system/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw
 
